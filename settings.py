1856a837e70b06ae4c945e948fd4b6261078eec3
mingina <mathea.mingina@busaracenter.org>
2019-12-16 14:28:40 +0300

    testing9 collecting static files

--- settings.py
+++ settings.py
@@ -146,7 +146,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = os.path.join(BASE_DIR, '__temp_static_root')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
