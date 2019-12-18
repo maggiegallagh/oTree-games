8cd31e2a8e316bbdf225bc7ccfd84793352eec59
Maggie Gallagher <maggiegallagh@gmail.com>
2019-12-17 23:30:25 -0800

    Revert "added self.timeout_seconds"
    This reverts commit 1a79e236fcafa9332a13b8cd7747ae280f138500.

--- counting_zeros_task/pages.py
+++ counting_zeros_task/pages.py
@@ -64,9 +64,6 @@ class zeroCount(Page):
 
 
 class ResultsWaitPage(WaitPage):
-    def __init__(self):
-        self.timeout_seconds = 5
-
     def get_timeout_seconds(self):
         return self.participant.vars['expiry'] - time.time()
 
@@ -74,6 +71,7 @@ class ResultsWaitPage(WaitPage):
         return self.get_timeout_seconds() > 5
 
     def after_all_players_arrive(self):
+
         self.group.check_count()
         self.group.count_correct_rounds()
 
