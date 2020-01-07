972775f1b20054222045b5e143f331d95da23172
Maggie Gallagher <maggiegallagh@gmail.com>
2020-01-06 23:48:45 -0800

    fixed survey questions

--- survey/pages.py
+++ survey/pages.py
@@ -6,19 +6,11 @@ from .models import Constants
 
 class Survey(Page):
     form_model = 'player'
-    form_fields = ['genderSlider', 'f_name', 'gender', 'age', 'race', 'optional_race', 'student', 'citizen', 'address',
+    form_fields = ['genderSlider', 'f_name', 'gender', 'age', 'race', 'optional_race', 'address',
                    'degree', 'school_name', 'school_state', 'major', 'collegeGPA', 'HITS']
-    # upcoming_apps = ['cannot_participate', 'thank_you_finished']
-
-    # def app_after_this_page(self, upcoming_apps=None):
-    #     if upcoming_apps is None:
-    #         upcoming_apps = ['cannot_participate', 'thank_you_finished']
-    #     return upcoming_apps[0]
 
     def app_after_this_page(self, upcoming_apps):
         return 'thank_you_finished'
-
-    # def app_after_this_page(self, upcoming_apps: List[str]) -> Optional[str]:
 
 
 page_sequence = [
