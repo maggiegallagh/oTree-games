1ee7322ba5bd3ecec56bb88b6b1e561a27bee150
Maggie Gallagher <maggiegallagh@gmail.com>
2020-01-07 21:14:50 -0800

    fixed currency payoffs for stroop 2

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ from os import environ
 # e.g. self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
-    real_world_currency_per_point=.10,
+    real_world_currency_per_point=1.00,
     participation_fee=0.00,
     doc="",
     mturk_hit_settings=dict(
